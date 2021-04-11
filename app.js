6a2d1e900e86fcdad85eebe1c3da9598d7feabd0
gar212 <gartoyou@gmail.com>
2021-04-11 16:59:42 +1200

    initial heroku push

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 5000;
+const port = 5000;
 
 // const database = null; // Database goes here
 
 app.get("/", (req,res) => res.send('Hello from backend'))
 
-app.listen(port, () => {
+app.listen(process.env.port || port, () => {
   console.log(`Server is listening on port ${port}`);
-});
+}); 
