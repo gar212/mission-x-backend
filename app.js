d4e62552af6c220733636913c533e233595619fd
gar212 <gartoyou@gmail.com>
2021-04-12 13:33:06 +1200

    add start script

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const PORT = 5000;
 
 // const database = null; // Database goes here
 
 app.get("/", (req,res) => res.send('Hello from backend'))
 
-app.listen(process.env.port || port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 }); 
