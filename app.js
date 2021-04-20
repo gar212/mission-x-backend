d1eeed86e82e7841a7522284af4e2b8aca833e02
gar212 <gartoyou@gmail.com>
2021-04-20 13:40:15 +1200

    add promise to query

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 const express = require('express');
 const db = require('./config/db');
-const mysql = require('mysql2');
 const cors = require('cors');
+const mysql = require('mysql2');
 
 const PORT = 5000;
 const app = express();
 app.use(cors());
 
-const runDBQuery = (sqlQuery) => {
-  const sqlQuery = `Select * from User WHERE UserID = 1`;
-  return db.promise().query(sqlQuery);
+
+const yourQuery = `SELECT * FROM User`;
+
+// A simple query
+const runDBQuery = (yourQuery) => {
+  return db.promise().query(yourQuery);
 };
 
+// A helper method to convert the DB query result into JSON format.
+const parseResultToJSON = (resultRowsArray) =>
+  resultRowsArray.map((mysqlObj) => Object.assign({}, mysqlObj));
+
+// When a API request is made to localhost:3000, the DB query is executed
 app.get("/", (req, res) => {
-  db.query(sql, (err, result) => {
-    if (err) {
-      console.log(err)
-    }
-    res.send(result);
-  })
-})
+  runDBQuery(yourQuery)
+    .then((queryResult, fields) => {
+      const [rows] = queryResult;
+      const jsonResults = parseResultToJSON(rows);
+      console.log("Sending response for GET", jsonResults);
+      // Sends the response if query was successful.
+      res.send(jsonResults);
+    })
+    .catch((error) => {
+      console.log(error);
+      // Sends an error if the query returned an error.
+      res.status(500).send(error);
+    });
+});
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-});
+})
