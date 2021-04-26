74ba7dce3c19e4eeaa442c8bad7c8a8105f233fe
Sam-McCathie <sammccathie2@gmail.com>
2021-04-26 12:55:52 +1200

    Added /projectbuilder db link

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const db = require('./config/db');
-const cors = require('cors');
-const mysql = require('mysql2');
+const express = require("express");
+const db = require("./config/db");
+const cors = require("cors");
+const mysql = require("mysql2");
 
 const PORT = 5000;
 const app = express();
@@ -16,21 +16,19 @@ const runDBQuery = (yourQuery) => {
 const parseResultToJSON = (resultRowsArray) =>
   resultRowsArray.map((mysqlObj) => Object.assign({}, mysqlObj));
 
-
 const getProfilePic = async (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
   const queryResult = await getUserProfilePic(id);
   const jsonResult = resultToJSON(queryResult);
 
-  const { image, mimeType } = jsonResult[0];
-  const encoding = 'base64';
+  const {image, mimeType} = jsonResult[0];
+  const encoding = "base64";
   // Data URIs - https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
   // Structure of the URI || data:[<mime type>][;charset=<charset>][;base64],<encoded data>
   // Example              || data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABNwAAAKmCAYAA...
   const uri = `data:${mimeType};${encoding},${image}`;
-  res.status(200).send({ dataURI: uri });
+  res.status(200).send({dataURI: uri});
 };
-
 
 app.get("/progresstracker", (req, res) => {
   const yourQuery = `SELECT * FROM User WHERE Role = 'student' `;
@@ -64,10 +62,29 @@ app.get("/studentprofiles", (req, res) => {
     });
 });
 
+/*TESTING */
+app.get("/projectbuilder", (req, res) => {
+  const yourQuery = `SELECT * FROM StudentDB.Project`;
+  runDBQuery(yourQuery)
+    .then((queryResult, fields) => {
+      const [rows] = queryResult;
+      const jsonResults = parseResultToJSON(rows);
+      // Sends the response if query was successful.
+      res.send(jsonResults);
+    })
+    .catch((error) => {
+      console.log(error);
+      // Sends an error if the query returned an error.
+      res.status(500).send(error);
+    });
+});
+
 // Starting the server after connecting to DB
 const startServer = async () => {
   try {
-    app.listen(process.env.PORT || PORT, () => console.log(`Server running on port ${PORT}!`));
+    app.listen(process.env.PORT || PORT, () =>
+      console.log(`Server running on port ${PORT}!`)
+    );
   } catch (e) {
     console.error(e);
   }
--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
-const mysql = require('mysql2');
-const fs = require('fs');
+const mysql = require("mysql2");
+const fs = require("fs");
 
 const db = mysql.createConnection({
-    host: "SG-missionX-4143-mysql-master.servers.mongodirector.com",
-    database: "StudentDB",
-    user: "Gar",
-    password: "Gar123456!",
-     ssl: {
-   ca: fs.readFileSync(
-   'cert.pem',
-   ),
- },
+  host: "SG-missionX-4143-mysql-master.servers.mongodirector.com",
+  database: "StudentDB",
+  user: "Sam",
+  password: "Sam123456!",
+  ssl: {
+    ca: fs.readFileSync("cert.pem"),
+  },
 });
 
 module.exports = db;
