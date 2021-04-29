b001f1a17dfcde8f62ef2f427e8a6db1a81f432b
RenoThompson <RThompson1708@gmail.com>
2021-04-29 18:16:17 +1200

    Added ProjectSubmissions Backend

--- app.js
+++ app.js
@@ -68,7 +68,28 @@ app.get("/projectbuilder", (req, res) => {
 });
 
 app.get("/helprequests", (req, res) => {
-  const yourQuery = `Select UserID, FirstName, LastName, ProfilePic, date_format(DateCreated, '%Y-%m-%d') as `date`, date_format(DateCreated, '%H:%i:%s') as `time`, HelpRequest.DateCreated from User left join HelpRequest on (User.UserID = HelpRequest.User_UserID) WHERE DateCreated is NOT NULL;`;
+  const yourQuery = `Select UserID, FirstName, LastName, ProfilePic, date_format(DateCreated, '%Y-%m-%d') as 'date', date_format(DateCreated, '%H:%i:%s') as 'time', HelpRequest.DateCreated from User left join HelpRequest on (User.UserID = HelpRequest.User_UserID) WHERE DateCreated is NOT NULL;`;
+  runDBQuery(yourQuery)
+    .then((queryResult, fields) => {
+      const [rows] = queryResult;
+      const jsonResults = parseResultToJSON(rows);
+      // Sends the response if query was successful.
+      res.send(jsonResults);
+    })
+    .catch((error) => {
+      console.log(error);
+      // Sends an error if the query returned an error.
+      res.status(500).send(error);
+    });
+});
+
+app.get("/projectsubmissions", (req, res) => {
+  const yourQuery = `SELECT UserID, FirstName, LastName, ProfilePic, date_format(DateSubmitted, '%Y-%m-%d') as 'date', date_format(DateSubmitted, '%H:%i:%s') as 'time', ProgressHistory.DateSubmitted, Project.Image 
+  from User 
+  left join HelpRequest on (User.UserID = HelpRequest.User_UserID)
+  left join  ProgressHistory on (User.UserID = ProgressHistory.User_UserID)
+  left join Project on (Project.ProjectID = ProgressHistory.Project_ProjectID)
+  WHERE DateCreated is NOT NULL;`;
   runDBQuery(yourQuery)
     .then((queryResult, fields) => {
       const [rows] = queryResult;
